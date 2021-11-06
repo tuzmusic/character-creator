40ba9bfb25a259ba72b5336346093025e0af6720
Jonathan Tuzman <tuzmusic@gmail.com>
2021-11-06 19:47:40 -0400

    Ex 4: Add perspective decoration, fix width for character.

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import CharacterEditor from './components/CharacterEditor';
 import Footer from './components/Footer';
+import styles from './App.module.css';
 
 function App() {
   return (
     <>
-      <CharacterEditor />
-      <Footer />
+      <div className={styles.perspectiveDecoration}/>
+      <CharacterEditor/>
+      <Footer/>
     </>
   );
 }
